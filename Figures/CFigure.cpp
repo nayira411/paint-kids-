17d104fac077ef9b5af0e672ae045109c3ad21c2
nayira411 <83923284+nayira411@users.noreply.github.com>
2024-01-04 03:30:26 +0200

    Add files via upload

---
+++ Figures/CFigure.cpp
@@ -0,0 +1,91 @@
+#include "CFigure.h"
+
+CFigure::CFigure(GfxInfo FigureGfxInfo)
+{ 
+	FigGfxInfo = FigureGfxInfo;	//Default status is non-filled.
+	Selected = false;
+	FigGfxInfo.isFilled = true;
+	PlayHidden = false;
+}
+
+CFigure::CFigure() {
+	PlayHidden = false;
+
+}
+
+void CFigure::SetSelected(bool s)
+{	Selected = s; }
+
+bool CFigure::IsSelected() const
+{	return Selected; }
+
+void CFigure::ChngDrawClr(color Dclr)
+{	FigGfxInfo.DrawClr = Dclr; }
+
+
+void CFigure::ChngFillClr(color Fclr)
+{	
+	FigGfxInfo.isFilled = true;
+	FigGfxInfo.FillClr = Fclr; 
+}
+
+//asmaa
+//This function takes a color object as an argument and returns its  string representation.(Convert from Color Type to String Type)
+string CFigure::ColorString(color ClrObj) const
+{
+	if (ClrObj == WHITE) return "WHITE";
+	else if (ClrObj == BLACK) return "BLACK";
+	else if (ClrObj == BROWN) return "BROWN";
+	else if (ClrObj == PURPLE) return "PURPLE";
+	else if (ClrObj == PINK) return "PINK";
+	else if (ClrObj == RED) return "RED";
+	else if (ClrObj == ORANGE) return "ORANGE";
+	else if (ClrObj == YELLOW) return "YELLOW";
+	else if (ClrObj == GREEN) return "GREEN";
+	else if (ClrObj == BLUE) return "BLUE";
+	else return "ORANGE";
+}
+//asma
+//This function takes a color object as an argument and returns its  string representation.(Convert from string to color object)
+color CFigure::ColorObject(string ClrStr) const
+{
+	if (ClrStr == "WHITE") return WHITE;
+	else if (ClrStr == "BLACK") return BLACK;
+	else if (ClrStr == "BROWN") return BROWN;
+	else if (ClrStr == "PURPLE") return PURPLE;
+	else if (ClrStr == "PINK") return PINK;
+	else if (ClrStr == "RED") return RED;
+	else if (ClrStr == "ORANGE") return ORANGE;
+	else if (ClrStr == "YELLOW") return YELLOW;
+	else if (ClrStr == "GREEN") return GREEN;
+	else if (ClrStr == "BLUE") return BLUE;
+	else return WHITE;
+}
+
+//this function returns the value of the IsHidden member variable of the FigGfxInfo
+bool CFigure::IsHidden() {
+	return FigGfxInfo.IsHidden;
+};
+//asma
+//to show the figure 
+void CFigure::show() {
+	FigGfxInfo.IsHidden = false;
+};
+
+void CFigure::Hide()
+{
+	PlayHidden = true;
+}
+void CFigure::ShowW()
+{
+	PlayHidden = false;
+}
+
+color CFigure::getFillColor()
+{
+	return FigGfxInfo.isFilled ? FigGfxInfo.FillClr : NULL;
+}
+
+bool CFigure::HiddenStatus() {
+	return PlayHidden;
+}
